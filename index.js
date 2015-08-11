9efe2b9c5028611847a9da397738465283178006
Mike Luby <mLuby@users.noreply.github.com>
2015-08-10 19:42:32 -0700

    refactor solve function

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ var config = require("./config");
 
 var move = actions.move.bind(actions);
 
-var moveNPieces = function(n, from, to, via) {
-  if (n === 1) {
-    move(from, to);
-  } else {
-    moveNPieces(n - 1, from, via, to);
-    move(from, to);
-    moveNPieces(n - 1, via, to, from);
-  }
-};
+function solve(n){
+  (function moveNPieces(n, from, to, via) {
+    if (n === 1) {
+      move(from, to);
+    } else {
+      moveNPieces(n - 1, from, via, to);
+      move(from, to);
+      moveNPieces(n - 1, via, to, from);
+    }
+  })(n, 0, 2, 1)
+}
 
-moveNPieces(config.pieces, 0, 2, 1);
+solve(config.pieces);
 actions.finish();
