3a6b215c16ea4c0b1069404956689aed7e918631
Mike Luby <mLuby@users.noreply.github.com>
2015-08-10 19:44:58 -0700

    prepare for meetup

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ var config = require("./config");
 var move = actions.move.bind(actions);
 
 function solve(n){
-  (function moveNPieces(n, from, to, via) {
-    if (n === 1) {
-      move(from, to);
-    } else {
-      moveNPieces(n - 1, from, via, to);
-      move(from, to);
-      moveNPieces(n - 1, via, to, from);
-    }
-  })(n, 0, 2, 1)
+  // fill in solution here
+
+
+
+
+
 }
 
 solve(config.pieces);
